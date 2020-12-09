83ec98d1159418993764c978da93ec2985709d96
victo <victorER98@gmail.com>
2020-12-09 15:05:55 +0100

    función frontier

--- R/frontier.R
+++ R/frontier.R
@@ -1,21 +1,31 @@
-#' @title EAT frontier
+#' @title Monoinput and monooutput EAT scenario
 #'
-#' @description This function creates a plot with EAT frontier for the scenario corresponding to one input and one output.
+#' @description This function creates a plot with the frontier estimated by EAT for the scenario corresponding to one input and one output.
 #'
-#' @param data Data used for modeling.
-#' @param tree List structure with the tree nodes.
-#' @param x Column input indexes in data.
-#' @param y Column output indexes in data.
-#' @param rownames logical. If T, row names are displayed in the plot instead of points.
+#' @param t An EAT object.
+#' @param train.data Logical. If T, training DMUs are displayed.
+#' @param train.color String. Color for training DMUs.
+#' @param pch Integer. Point shape.
+#' @param rownames Logical. If T, row names are displayed instead of points.
+#' @param rwn.size Integer. Rowname size
 #'
 #' @importFrom ggplot2 ggplot aes_string geom_point geom_line geom_text xlab ylab
 #' @importFrom conflicted conflict_prefer
 #'
+#' @return Scatter plot with DMUs and the frontier predicted by EAT.
+#' 
 #' @export
-#'
-#' @return Scatter plot with DMUs and frontier predicted by `EAT`
-frontier <- function(data, tree, x, y, rownames = FALSE){
+frontier <- function(t, train.data = FALSE, train.color = "black", pch = 19, rownames = FALSE, 
+                     rwn.size = 3){
   conflict_prefer("predict", "eat")
+  
+  t_data <- t[["data"]][["data"]]
+  rownames(t_data) <- t[["data"]][["row_names"]]
+  x <- t[["data"]][["x"]]
+  y <- t[["data"]][["y"]]
+  tree <- t[["tree"]]
+  x_names <- t[["data"]][["input_names"]]
+  y_names <- t[["data"]][["output_names"]]
 
   if(length(x) > 1 || length(y) > 1){
     stop(cat("More than one input or one output are not allowed"))
@@ -23,23 +33,36 @@ frontier <- function(data, tree, x, y, rownames = FALSE){
   } else {
 
     x_values <- NULL
-
-    fp <- data.frame(x_values = seq(min(data[, x]), max(data[, x]),  length.out = 2000),
-                     frontier = 0)
-    pred <- predict(tree, fp, 1, 2)
+    
+    fp <- data.frame(x_values = seq(min(t_data[, x]), max(t_data[, x]),  length.out = 2000))
+    names(fp) <- x_names
+    pred <- predict(t, fp)
     fp[, "frontier"] <- pred
+    names(fp)[1] <- "x1"
 
-    plot <- ggplot(data) +
-     geom_line(data = fp, aes(x = x_values, y = frontier), size = 1, colour = "turquoise4") +
-      xlab(names(data)[x]) +
-      ylab(names(data)[y])
-
-    if (rownames == T){
+    plot <- ggplot(t_data) +
+     geom_line(data = fp, aes(x = x1, y = frontier), size = 1, colour = "turquoise4") +
+      xlab(x_names) +
+      ylab(y_names)
+    
+    if (train.data == FALSE){
       plot <- plot +
-        geom_text(aes_string(x = names(data)[x], y = names(data)[y], label = "rownames(data)"))
+        xlim(c(min(fp$x1), max(fp$x1))) +
+        ylim(c(min(fp$frontier) - (min(fp$frontier) / 10), max(fp$frontier)))
     } else {
-      plot <- plot +
-        geom_point(aes_string(x = names(data)[x], y = names(data)[y]))
+      if (rownames == T){
+        plot <- plot +
+          geom_text(aes_string(x = x_names, 
+                               y = y_names, 
+                               label = "rownames(t_data)"),
+                    color = train.color,
+                    size = rwn.size)
+      } else {
+        plot <- plot + 
+          geom_point(aes_string(x =x_names, y = y_names),
+                     color = train.color,
+                     pch = pch)
+      }
     }
   }
   return(plot)

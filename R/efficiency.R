42d277763ee3c34a17baf3bd9fc75422f9684f2c
victo <victorER98@gmail.com>
2020-12-11 13:52:47 +0100

    efficiency_density

--- R/efficiency.R
+++ R/efficiency.R
@@ -400,17 +400,17 @@ efficiency_scores <- function(object, scores_model, r = 4) {
   V1 <- NULL
 
   scores <- as.data.frame(scores)
-  names(scores) <- "Score"
+  names(scores) <- scores_model
   rownames(scores) <- object[["data"]][["row_names"]]
 
   descriptive <- scores %>%
-    summarise("Mean" = round(mean(scores$Score), 2),
-              "Std. Dev." = round(sd(scores$Score), 2),
-              "Min" = round(min(scores$Score), 2),
-              "Q1" = round(quantile(scores$Score)[[2]], 2),
-              "Median" = round(median(scores$Score), 2),
-              "Q3" = round(quantile(scores$Score)[[3]], 2),
-              "Max" = round(max(scores$Score), 2)
+    summarise("Mean" = round(mean(scores[, 1]), 2),
+              "Std. Dev." = round(sd(scores[, 1]), 2),
+              "Min" = round(min(scores[, 1]), 2),
+              "Q1" = round(quantile(scores[, 1])[[2]], 2),
+              "Median" = round(median(scores[, 1]), 2),
+              "Q3" = round(quantile(scores[, 1])[[3]], 2),
+              "Max" = round(max(scores[, 1]), 2)
               )
   
   print(kable(descriptive), "pipe")
@@ -418,174 +418,125 @@ efficiency_scores <- function(object, scores_model, r = 4) {
   return(round(scores, r))
 }
 
-#' @title Efficiency Scores Bar Plot
+#' @title Efficiency Scores Jitter Plot
 #'
-#' @description This function returns a bar plot with scores
+#' @description This function returns a jitter plot. DMUs are grouped in nodes (same input paradigm) and its score are showed. A black point represents the score mean in a group and the line the standard deviation. Finally, the user can specify an upper bound (upb) and a lower bound (lwb) in order to show the labels.
 #'
 #' @param object An EAT object.
 #' @param scores Dataframe with scores.
-#' @param color Logical. If True, observations with same efficient level output are painted in the same color.
+#' @param upb Numeric. Upper bound for labeling.
+#' @param lwb Numeric. Lower bound for labeling.
 #'
-#' @importFrom ggplot2 ggplot aes geom_col theme element_text labs
-#' @importFrom stats median quantile sd
+#' @importFrom ggplot2 ggplot aes geom_jitter stat_summary position_jitter
+#' @importFrom dplyr %>% select
+#' @importFrom ggrepel geom_text_repel
 #'
 #' @export
 #'
-#' @return Bar plot with efficiency scores
-efficiency_barplot <- function(object, scores, color = T) {
+#' @return Geom jitter plot with DMUs and scores.
+efficiency_jitter <- function(object, scores, upb = 1, lwb = 0) {
   
-  if(color == T){
-    
-    groups <- object[["nodes_df"]][["leafnodes_df"]][, c(1, 6)]
-    
-    scores_df <- data.frame(scores = scores,
-                            Group = NA)
-    
-    rownames(scores_df) <- object[["data"]][["row_names"]]
-    
-    for (i in 1:nrow(scores_df)){
-      scores_df[groups[i, "index"][[1]], "Group"] <- groups[i, "id"]
-    }
-    
-    efficiency_histogram <- ggplot(scores_df,
-                                   aes(x = reorder(row.names(scores_df), - Score), y = Score,
-                                       fill = as.factor(Group))) +
-      geom_col() +
-      theme(axis.text.x = element_text(angle = 45), legend.position = "bottom") +
-      xlab("DMU") +
-      ylab("Score") + 
-      labs(fill = "Group")
-      # scale_fill_brewer(palette = "Set1")
-      # scale_alpha_discrete(range = c(0.4, 0.9))
-  } else {
-    
-    efficiency_histogram <- ggplot(scores_df,
-                                   aes(x = reorder(row.names(scores_df), - Score), y = Score)) +
-      geom_col() +
-      theme(axis.text.x = element_text(angle = 45)) +
-      xlab("DMU") +
-      ylab("Score")
+  id <- index <- Group <- Score <- NULL
+  
+  groups <- object[["nodes_df"]][["leafnodes_df"]] %>%
+    select(id, index)
+  
+  scores_df <- data.frame(Score = scores,
+                          Group = NA)
+  
+  names(scores_df) <- c("Score", "Group")
+  
+  rownames(scores_df) <- object[["data"]][["row_names"]]
+  
+  for (i in 1:nrow(scores_df)){
+    scores_df[groups[i, "index"][[1]], "Group"] <- groups[i, "id"]
   }
   
-  return(efficiency_histogram)
+  scores_df$Group <- as.factor(scores_df$Group)
+  
+  data_summary <- function(x) {
+    m <- mean(x)
+    ymin <- m - sd(x)
+    ymax <- m + sd(x)
+    return(c(y = m, ymin = ymin, ymax = ymax))
+  }
+  
+  jitter_plot <- ggplot(scores_df, aes(x = Group, y = Score, color = Group)) + 
+    geom_jitter(position = position_jitter(0.2), size = 2) +
+    stat_summary(fun.data = data_summary, color = "black") +
+    geom_text_repel(aes(label = ifelse(Score >= lwb & Score <= upb, 
+                                       rownames(scores_df), "")))
+  
+  return(jitter_plot)
 }
 
-#' @title Efficiency Density and Barplot
+#' @title Efficiency Scores Density Plot
 #'
-#' @description This function returns a density plot
+#' @description Density plot for scores. EAT scores in red and FDH scores in blue.
 #'
-#' @param scores Score object
-#' @param data Data to be used.
-#' @param tree Tree structure.
-#' @param x Column input indexes in data.
-#' @param y Column output indexes in data.
+#' @param object An EAT object.
+#' @param scores Dataframe with scores.
+#' @param FDH Logical. If FDH = TRUE, density plot for FDH is showed too.
 #'
-#' @importFrom ggplot2 ggplot aes geom_histogram  geom_density
-#' @importFrom dplyr  %>% mutate left_join group_by
-#' @importFrom stats median quantile sd
+#' @importFrom ggplot2 ggplot aes_string geom_histogram geom_density
 #'
 #' @export
 #'
-#' @return Efficiency scores
-efficiency_density_barplot <- function(scores, data, tree, x, y) {
+#' @return Density plot for scores
+efficiency_density <- function(object, scores, FDH = TRUE) {
   
-  efficiency_density <- ggplot(scores, aes(V1)) +
-      geom_histogram(aes(y = ..density..), colour = "black", fill = "grey")+
-      geom_density(alpha = .3, fill = "#FF583A", colour = "#FF583A") +
-      xlab("Score") +
-      ylab("Frequency")
-
-  return(efficiency_density_barplot)
-}
-
-#' @title Efficiency Density FDH and EAT
-#'
-#' @description This function returns a density plot
-#'
-#' @param scores Score object
-#' @param data Data to be used.
-#' @param tree Tree structure.
-#' @param x Column input indexes in data.
-#' @param y Column output indexes in data.
-#'
-#' @importFrom ggplot2 ggplot aes geom_histogram  geom_density
-#' @importFrom dplyr  %>% mutate left_join group_by
-#' @importFrom stats median quantile sd
-#'
-#' @export
-#'
-#' @return Efficiency scores
-efficiency_density <- function(scores, data, tree, x, y) {
-  
-  efficiency_density <- ggplot(scores, aes(V1)) +
-    geom_density(alpha = .3, fill = "#FF583A", colour = "#FF583A") +
+  efficiency_density <- ggplot() +
+    geom_density(data = scores, aes_string(names(scores)[1]), alpha = .3, 
+                 fill = "#FF583A", colour = "#FF583A") +
     xlab("Score") +
-    ylab("Frequency")
+    ylab("Density")
+  
+  if (FDH == TRUE){
+    
+    data <- object[["data"]][["data"]]
+    x <- object[["data"]][["x"]]
+    y <- object[["data"]][["y"]]
+    
+    j <- N_leaves <- nrow(data)
+    FDH_scores <- matrix(nrow = j, ncol = 1)
+    x_k <- atreeTk <- as.matrix(data[, x])
+    y_k <- ytreeTk <- as.matrix(data[, y])
+    nX <- length(x)
+    nY <- length(y)
+    
+    if (names(scores) == "EAT_BCC_out"){
+      scores_FDH <- EAT_BCC_out(j, FDH_scores, x_k, y_k, atreeTk, ytreeTk, nX, nY, N_leaves)
+      
+    } else if (names(scores) == "EAT_BCC_in"){
+      scores_FDH <- EAT_BCC_in(j, scores, x_k, y_k, atreeTk, ytreeTk, nX, nY, N_leaves)
+      
+    } else if (names(scores) == "EAT_DDF"){
+      scores_FDH <- EAT_DDF(j, scores, x_k, y_k, atreeTk, ytreeTk, nX, nY, N_leaves)
+      
+    } else if (names(scores) == "EAT_RSL_out"){
+      scores_FDH <- EAT_RSL_out(j, scores, x_k, y_k, atreeTk, ytreeTk, nX, nY, N_leaves)
+      
+    } else if (names(scores) == "EAT_RSL_in"){
+      scores_FDH <- EAT_RSL_in(j, scores, x_k, y_k, atreeTk, ytreeTk, nX, nY, N_leaves)
+      
+    } else if (names(scores) == "EAT_WAM"){
+      scores_FDH <- EAT_WAM(j, scores, x_k, y_k, atreeTk, ytreeTk, nX, nY, N_leaves)
+    }
+    
+    scores_FDH <- as.data.frame(scores_FDH)
+    
+    names(scores_FDH) <- "scores_FDH"
+  
+    efficiency_density <- efficiency_density + 
+      geom_density(data = scores_FDH, aes_string(x = "scores_FDH"), 
+                   alpha = .3, 
+                   fill = "#00AFBB", colour = "#00AFBB") 
+  }
   
   return(efficiency_density)
 }
 
-#' @title Free Disposal Hull predictor
-#'
-#' @description ...
-#'
-#' @param data Data frame or matrix.
-#' @param x Column input indexes in data.
-#' @param y Column output indexes in data.
-#' 
-#' @export
-#'
-#' @value ...
-yFDH <- function(data, x, y) {
-  
- data <- preProcess(data, x, y)
- 
- # Size data
- N <- nrow(data)
- 
- # Reorder index 'x' and 'y' in data
- x <- 1:((ncol(data) - 1) - length(y))
- y <- (length(x) + 1):(ncol(data) - 1)
- 
- data <- data %>% select(-id)
-  
- data[, "yFDH"] <- 0
- 
- for (i in 1:nrow(data)){
-   data[i, "yFDH"] <- FDH(data, data[i, x], y)
- }
- 
- return(data)
- 
-}
 
-#' @title Free Disposal Hull
-#'
-#' @description ...
-#'
-#' @param data Data frame or matrix.
-#' @param xarray Input columns
-#' @param yarray Output columns
-#' 
-#' @export
-#'
-#' @value ...
-FDH <- function(data, xarray, y){
-  yMax <- -Inf
-  
-  for (n in 1:nrow(data)){
-    newMax <- TRUE
-    for (j in 1:length(xarray)){
-      if (data[n, j] > xarray[j]){
-        newMax <- FALSE
-        break
-      }
-    }
-    if (newMax && yMax < data[n, y]) yMax <- data[n, y]
-  }
-  
-  return(yMax)
-}
 
 
 
